0620bf3342c1ef8b91a74c4eb4ec1f9050e82a17
gabrielTor <gabriel_torres2012@yahoo.com>
2024-03-13 00:35:00 -0300

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="text-white">
+      <header className="text-xl md:text-5xl font-semibold text-center py-20">
+        Sorry but this page doesn't exist
+      </header>
+    </div>
+  );
+}
