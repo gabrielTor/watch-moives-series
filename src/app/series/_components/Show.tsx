cd2dcfbbad37b14b3d5a9b7638d1e2057592d143
gabrieltor <dariogabrieltorres93@gmail.com>
2024-04-22 19:50:22 -0300

    fix ( series-page missing max-w )

--- src/app/series/_components/Show.tsx
+++ src/app/series/_components/Show.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Show() {
-  return <div>Coming soon!</div>;
+  return (
+    <div className="grid place-items-center h-[100dvh] text-3xl">
+      Coming soon!
+    </div>
+  );
 }
--- src/app/series/page.tsx
+++ src/app/series/page.tsx
@@ -10,7 +10,7 @@ export default async function page({ searchParams }: Readonly<Props>) {
   const tv = await getSeries(searchParams.page);
 
   return (
-    <section className="p-4 max-w-7xl mx-auto">
+    <section className="p-4 max-w-8xl mx-auto">
       <Movies movies={tv?.results} path="series" />
       <PageNumbers amountOfPages={tv!.total_pages} currentPage={tv!.page} />
     </section>
