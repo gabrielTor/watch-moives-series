60db221322bca830df0a36f3c2476f1cb7c38851
gabrieltor <dariogabrieltorres93@gmail.com>
2024-04-22 00:57:03 -0300

    feat ( skeleton-loading )

--- src/app/movie/[movieId]/loading.tsx
+++ src/app/movie/[movieId]/loading.tsx
@@ -1,24 +1,26 @@
 const mockUpLoadingText = [
-  { className: "w-full h-5 rounded", key: 123 },
-  { className: "w-full h-5 rounded", key: 456 },
-  { className: "w-full h-5 rounded", key: 789 },
-  { className: "w-full h-5 rounded", key: 101123 },
-  { className: "w-full h-5 rounded", key: 456789 },
+  { className: "w-[10%] h-5 rounded animate-pulse bg-navy", key: 123 },
+  { className: "w-full h-5 rounded animate-pulse bg-navy", key: 456 },
+  { className: "w-[10%] h-5 rounded animate-pulse bg-navy", key: 789 },
+  { className: "w-[20%] h-5 rounded animate-pulse bg-navy", key: 101123 },
+  { className: "w-[15%] h-5 rounded animate-pulse bg-navy", key: 456789 },
+  {
+    className:
+      "w-full mt-4 md:flex-grow md:h-auto h-96 rounded animate-pulse bg-navy",
+    key: 8949,
+  },
 ];
 
 export default function Loading() {
   return (
     <div className="p-4">
-      <span className="animate-pulse w-full h-5 mb-2 bg-navy" />
+      <div className="animate-pulse w-full md:w-1/3 h-5 mb-2 bg-navy rounded" />
       <div className="flex flex-wrap">
-        <div className="w-full md:w-1/3">
-          <span className="animate-pulse w-full h-[1000px] bg-navy" />
-        </div>
-        <div className="w-full md:w-2/3 md:pl-4 flex flex-col">
+        <span className=" md:w-1/3 animate-pulse w-full h-[50dvh] md:h-[80dvh] bg-navy rounded" />
+        <div className="w-full md:w-2/3 md:pl-4 flex flex-col gap-4 mt-4 md:mt-0">
           {mockUpLoadingText.map((line) => (
             <p className={line.className} key={line.key} />
           ))}
-          <span className="animate-pulse h-[1000px] bg-navy" />
         </div>
       </div>
     </div>
