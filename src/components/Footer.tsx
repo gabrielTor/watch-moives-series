34b26e364faab7674723f9f50d5b1543aad7ed21
gabrielTor <gabriel_torres2012@yahoo.com>
2024-05-12 02:24:01 -0300

    feat ( footer )

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,20 @@
+import Link from "next/link";
+
+const portfolioLink = "https://gabrieltor.github.io/portfolio/";
+
 export default function Footer() {
-  return <footer className="h-20 bg-navy"></footer>;
+  return (
+    <footer className="h-20 bg-navy text-gray-300 text-sm grid place-items-center">
+      <span className="flex gap-2">
+        Designed and developed by Gabriel Torres -{" "}
+        <Link
+          className="hover:text-liteBlue"
+          href={portfolioLink}
+          target="_blank"
+        >
+          DGT
+        </Link>
+      </span>
+    </footer>
+  );
 }
